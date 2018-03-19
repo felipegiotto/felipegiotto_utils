af94636767bc61e354b9b7d6997ff01639e11c0a
Felipe Giotto - MBP <felipegiotto@gmail.com>
2018-03-19 09:22:31 -0300

    Carregando arquivos properties utilizando java.nio

--- src/main/java/com/felipegiotto/utils/FGFileUtils.java
+++ src/main/java/com/felipegiotto/utils/FGFileUtils.java
@@ -4,8 +4,16 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FilenameFilter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Properties;
+
+//import org.apache.logging.log4j.LogManager;
+//import org.apache.logging.log4j.Logger;
 
 /**
  * Métodos auxiliares referentes a arquivos ou pastas
@@ -14,12 +22,13 @@ import java.util.Properties;
  */
 public class FGFileUtils {
 	
+	//private static final Logger LOGGER = LogManager.getLogger(FGFileUtils.class);
+	
 	/**
 	 * Carrega um objeto Properties de um arquivo
 	 * 
-	 * TODO: Criar uma classe (ex: FGFileProperties), que já receba um arquivo na sua inicialização e, a cada chamada ao método "saveToFile", reescreve.
+	 * TODO: Criar uma classe (ex: FGFileProperties), que já receba um arquivo na sua inicialização e, a cada chamada ao método "saveToFile", reescreve. Permitir salvar somente se alguma propriedade foi modificada. Criar gets e sets para diversos tipos de dados (ex: int, float, double), permitindo gravar e ler NULL (nesse caso, grava string vazia no arquivo e permite a carga posterior)
 	 * TODO: Criar FGDateUtils, com formatadores "padrão" para diversos formatos conhecidos, sempre utilizando SafeSimpleDateFormat (SQL Date, SQL Timestamp, D/M/Y, D/M/Y/H/M/S, H/M/S)
-	 *   
 	 * @param arquivo : arquivo de entrada, com os dados que serão populados no objeto
 	 * @param obrigatorio : se "true" e se o arquivo de entrada não existir, lança uma exceção
 	 * @return
@@ -39,6 +48,30 @@ public class FGFileUtils {
 
 	
 	/**
+	 * Carrega um objeto Properties de um arquivo
+	 * 
+	 * TODO: Criar uma classe (ex: FGFileProperties), que já receba um arquivo na sua inicialização e, a cada chamada ao método "saveToFile", reescreve.
+	 * TODO: Criar FGDateUtils, com formatadores "padrão" para diversos formatos conhecidos, sempre utilizando SafeSimpleDateFormat (SQL Date, SQL Timestamp, D/M/Y, D/M/Y/H/M/S, H/M/S)
+	 *   
+	 * @param arquivo : arquivo de entrada, com os dados que serão populados no objeto
+	 * @param obrigatorio : se "true" e se o arquivo de entrada não existir, lança uma exceção
+	 * @return
+	 * @throws IOException
+	 */
+	public static Properties carregarArquivoProperties(Path arquivo, boolean obrigatorio) throws IOException {
+		Properties props = new Properties();
+		if (Files.isRegularFile(arquivo)) {
+			try (InputStream fis = Files.newInputStream(arquivo)) {
+				props.load(fis);
+			}
+		} else if (obrigatorio) {
+			throw new FileNotFoundException("Arquivo não existe: " + arquivo);
+		}
+		return props;
+	}
+	
+	
+	/**
 	 * Grava um objeto Properties em um arquivo.
 	 * @param properties
 	 * @param arquivoSaida
@@ -53,6 +86,21 @@ public class FGFileUtils {
 	}
 
 
+	/**
+	 * Grava um objeto Properties em um arquivo.
+	 * @param properties
+	 * @param arquivoSaida
+	 * @param comentario : texto inserido na primeira linha do arquivo
+	 * @throws IOException
+	 */
+	public static void salvarArquivoProperties(Properties properties, Path arquivoSaida, String comentario)
+			throws IOException {
+		try (OutputStream fos = Files.newOutputStream(arquivoSaida)) {
+			properties.store(fos, comentario);
+		}
+	}
+	
+	
 	public static void garantirQuePastaExista(File in) throws IOException {
 		if (!in.isDirectory()) {
 			throw new IOException("Pasta não existe: " + in);
