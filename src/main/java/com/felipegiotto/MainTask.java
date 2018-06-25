613d3c213285e7cb72103dff8490c5f5be328aa4
Felipe Luiz Christofolli Giotto <felipe.giotto@trt4.jus.br>
2018-06-25 12:45:38 -0300

    Tarefa inicial

---
+++ src/main/java/com/felipegiotto/MainTask.java
@@ -0,0 +1,17 @@
+package com.felipegiotto;
+
+import com.felipegiotto.utils.FGEyeFatigueUtils;
+import com.felipegiotto.utils.FGInterativoUtils;
+
+public class MainTask {
+
+	public static void main(String[] args) throws Exception {
+		System.out.println("felipegiotto_utils");
+		switch (FGInterativoUtils.perguntarOpcoesParaUsuario("Qual classe deve ser executada?", 
+				"FGEyeFatigueUtils - Avisa a cada 20 minutos para que seja realizada uma pausa no trabalho")) {
+		case 1: 
+			FGEyeFatigueUtils.main(null);
+			break;
+		}
+	}
+}
